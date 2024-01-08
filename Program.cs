11939d43eaa9253f631d08d06587dfbcb7187a66
David Carroll <carroll.david.s@gmail.com>
2024-01-07 20:24:55 -0600

    Implement prime factorization program - add readme - add gitignore

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Prime Factorization Program");
+
+Console.Write("Enter a positive integer greater than 1: ");
+
+var number = int.Parse(Console.ReadLine()!);
+
+if (number > 1)
+{
+    var factors = GetPrimeFactors(number);
+
+    Console.Write($"Prime factors of {number}: ");
+
+    foreach (var factor in factors)
+    {
+        Console.Write(factor + " ");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input.");
+}
+
+List<int> GetPrimeFactors(int n)
+{
+    List<int> factors = new();
+    var divisor = 2;
+
+    while (n > 1)
+    {
+        if (n % divisor == 0)
+        {
+            factors.Add(divisor);
+            n /= divisor;
+        }
+        else
+        {
+            divisor++;
+        }
+    }
+
+    return factors;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Prime Factorization Program
+
+This program will take a number greater than 1 and return all of the prime factors of that number.
